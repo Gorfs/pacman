7062210c0d99b1fbfa2783646aedd3ec96cbfba5
rabhivid <noam@noam-virtual-machine>
2023-09-27 10:50:04 +0200

    singleton

--- src/main/java/model/PacMan.java
+++ src/main/java/model/PacMan.java
@@ -12,8 +12,20 @@ public final class PacMan implements Critter {
 
     private PacMan() {
     }
-
-    public static final PacMan INSTANCE = new PacMan();
+    /**On pourra mettre dans le menu le choix du nom qui s'affichera au dessus du pacman,
+     * en plus si on refait un round, le pacman garde quand meme le nom en l'initialisant
+     */
+    public static PacMan INSTANCE = new PacMan();
+    public String name;
+    private PacMan(String name){
+        this.name=name;
+    }
+    public static PacMan getInstance(String name){
+        if(INSTANCE==null){
+            INSTANCE=new PacMan(name);
+        }
+        return INSTANCE;
+    }
 
     @Override
     public RealCoordinates getPos() {
