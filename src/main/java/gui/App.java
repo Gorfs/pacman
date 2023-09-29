a5e7f549dcffc5a724a43b1f235def3098ff8552
BEALES archie <archie.beales@etu.u-paris.fr>
2023-09-29 20:49:59 +0200

    Revert "Merge branch 'ma' into 'develop'"
    This reverts merge request !3

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -1,21 +1,16 @@
 package gui;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.nio.file.Paths;
-
 import config.MazeConfig;
 import model.MazeState;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        music();
         var root = new Pane();
         var gameScene = new Scene(root);
         var pacmanController = new PacmanController();
@@ -27,14 +22,4 @@ public class App extends Application {
         primaryStage.show();
         gameView.animate();
     }
-
-    MediaPlayer mediaPlayer;
-    public void music(){
-        String s = "src\\main\\resources\\bgm.mp3";
-        Media h = new Media(Paths.get(s).toUri().toString());
-        mediaPlayer = new MediaPlayer(h);
-        mediaPlayer.play();
-    }
-        
-    
 }
--- src/main/java/model/MazeState.java
+++ src/main/java/model/MazeState.java
@@ -6,14 +6,10 @@ import geometry.IntCoordinates;
 import geometry.RealCoordinates;
 import misc.Debug;
 
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
 import static model.Ghost.*;
-
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public final class MazeState {
     private final MazeConfig config;
@@ -121,7 +117,6 @@ public final class MazeState {
                     resetCritter(critter);
                 } else {
                     playerLost();
-                    music_death(); 
                     return;
                 }
             }
@@ -168,24 +163,4 @@ public final class MazeState {
     public boolean getGridState(IntCoordinates pos) {
         return gridState[pos.y()][pos.x()];
     }
-
-    // ...
-
-    MediaPlayer mediaPlayer;
-
-
-    public void music_score(){
-        String s = "src\\main\\resources\\score.mp3";
-        Media h = new Media(Paths.get(s).toUri().toString());
-        mediaPlayer = new MediaPlayer(h);
-        mediaPlayer.play();
-    }
-
-    public void music_death(){
-        String s = "src\\main\\resources\\death.mp3";
-        Media h = new Media(Paths.get(s).toUri().toString());
-        mediaPlayer = new MediaPlayer(h);
-        mediaPlayer.play();
-    }
-
 }
