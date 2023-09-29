e7685fbf670a1a667338d955e26a58bf21b566ba
archie beales <archiebeales@gmail.com>
2023-09-29 11:53:38 +0200

    Made the text white and also replaced the livesText label with an image of minecraft hearts to reperesent lives

--- src/main/java/gui/Menu.java
+++ src/main/java/gui/Menu.java
@@ -1,15 +1,20 @@
 package gui;
 
 import geometry.IntCoordinates;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import model.MazeState;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Menu {
 
     private final double scale;
     private int score;
+    private double size = 0.5;
 
     public Menu(double scale){
         this.scale = scale;
@@ -23,15 +28,16 @@ public class Menu {
 
         Label scoreText = new Label("Score:" + String.valueOf(score) );
         Label livesText = new Label("Current lives: " + String.valueOf(MazeState.getLives()));
+        ImageView livesImage = new ImageView( new Image("heart3.png",scale*size , scale * size, true, true));
         
         HBox scoreHb = new HBox();
         HBox livesHb = new HBox();
         scoreHb.getChildren().addAll(scoreText);
-        livesHb.getChildren().addAll(livesText);
+        livesHb.getChildren().addAll(livesImage);
         menu.getChildren().addAll(scoreHb, livesHb);
         
-        menu.setSpacing(30);
-        livesText.setStyle("-fx-text-fill: white; -fx-font-size: 16px;");
+        menu.setSpacing(20);
+        livesText.setTextFill(Color.rgb(154, 155, 155));
 
         // Failed attempt to make the score text white
         // scoreText.setTextFill(Paint.valueOf("#000"));
@@ -45,11 +51,21 @@ public class Menu {
                 score = MazeState.getScore();
                 scoreHb.getChildren().remove(0);
                 livesHb.getChildren().remove(0);
-                scoreHb.getChildren().add(new Label("score :" + String.valueOf(score)));
-                livesHb.getChildren().add(new Label("Lives :" + String.valueOf(MazeState.getLives())));
+
+               // Label livesText = new Label("Lives : " + String.valueOf(MazeState.getLives()));
+               
+                Label scoreText = new Label("Score : " + String.valueOf(MazeState.getScore()));
+                ImageView livesImage = new ImageView( new Image(("heart" + String.valueOf(MazeState.getLives()) + ".png"),scale*size , scale * size, true, true));
                 
-                menu.setTranslateX(pos.x()-30);
-                menu.setTranslateY(pos.y());
+                // livesText.setStyle("-fx-text-fill: white; -fx-font-size: 16px;");
+                scoreText.setStyle("-fx-text-fill: white; -fx-font-size: 16px;");
+
+                scoreHb.getChildren().add(scoreText);
+                livesHb.getChildren().add(livesImage);
+                
+                scoreHb.setAlignment(Pos.BOTTOM_RIGHT);
+                livesHb.setAlignment(Pos.BOTTOM_LEFT);
+
 
             }
 
