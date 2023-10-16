ffe0dff3c7075a767e417cebc3dfeeb352c7753a
MA <mamathieu3bg3@gmail.com>
2023-10-16 21:55:27 +0200

    Updated changes in src directory

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
 
     public void playBackgroundMusic() {
         try {
-            File audioFile = new File("src/main/resources/bgm.wav"); //test
+            File audioFile = new File("src/main/resources/bgm.wav"); 
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
