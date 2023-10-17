8f09f26c69c49e16d9b13ec6459675cb5fc3794a
cheze <remi.cheze@etu.u-paris.fr>
2023-10-17 09:57:57 +0200

    Fixed the merge request BGM

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -47,16 +47,17 @@ public class App extends Application {
 
     public void playBackgroundMusic() {
         try {
-            File audioFile = new File("src/main/resources/bgm.wav"); 
+            File audioFile = new File("src/main/resources/bgm.wav");
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            gainControl.setValue(0.2f); 
+            gainControl.setValue(0.2f);
             clip.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
         
 
     private static KeyCode[] k = {KeyCode.LEFT,KeyCode.RIGHT,KeyCode.UP,KeyCode.DOWN};
