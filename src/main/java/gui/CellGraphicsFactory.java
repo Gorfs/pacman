43f2f06cd79d62094bfe3c527bcdf701ac56730d
CAO VAN TRUONG alexis <alexis.cao-van-truong@etu.u-paris.fr>
2023-12-05 01:21:57 +0100

    Revert "première version d'ajout de collectibles bonus #58"
    This reverts commit b06d73ea0bbce479dbef21ce740b3ce0cdb21e5f

--- src/main/java/gui/CellGraphicsFactory.java
+++ src/main/java/gui/CellGraphicsFactory.java
@@ -8,8 +8,6 @@ import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import model.MazeState;
 
 
@@ -31,8 +29,7 @@ public class CellGraphicsFactory {
             case DOT -> scale/10;
             case ENERGIZER -> scale/5;
             case NOTHING -> 0;
-            case WALL -> scale/4;
-            default -> 0;});
+            case WALL -> scale/4;});
 
         dot.setCenterX(scale/2);
         dot.setCenterY(scale/2);
@@ -69,12 +66,6 @@ public class CellGraphicsFactory {
                     group.getChildren().add(wall);
                 }
             }
-        } else if (cell.initialContent() == Cell.Content.CHERRY){
-            var cherryImage = new Image(getClass().getResourceAsStream("/cherry.png"));
-            var cherryImageView = new ImageView(cherryImage);
-            cherryImageView.setFitWidth(scale);
-            cherryImageView.setFitHeight(scale);
-            group.getChildren().add(cherryImageView);
         } else {
             // If there isn't a wall in the cell
             dot.setFill(Color.YELLOW);
@@ -121,7 +112,7 @@ public class CellGraphicsFactory {
         return new GraphicsUpdater() {
             @Override
             public void update(long deltaT) {
-                group.setVisible(!state.getGridState(pos));
+                dot.setVisible(!state.getGridState(pos));
             }
 
             @Override
