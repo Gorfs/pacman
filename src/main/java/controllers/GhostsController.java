34b2e9d1592ee9126480b2751cc7466eb723b2d8
JIN christophe <christophe.jin@etu.u-paris.fr>
2024-01-09 19:43:57 +0100

    Revert "Merge branch 'develop' into Noam"
    This reverts commit 0fa991aaa519134ac99fb6d2cbc38129bac35ffa

--- src/main/java/controllers/GhostsController.java
+++ src/main/java/controllers/GhostsController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import config.Cell;
-import config.Constants;
 import config.MazeConfig;
 import geometry.IntCoordinates;
 import geometry.RealCoordinates;
@@ -21,11 +20,7 @@ public sealed abstract class GhostsController permits BlinkyController, ClydeCon
     // timer to update scatter mode
     float timer = 0;
     // If it can start chasing/scatter
-    boolean started = false; 
-
-    int[] startPosition1;
-    int[] startPosition2;
-    int[] startPosition3;
+    boolean started = false;
 
     /**
      * Method that start the ghost AI.
@@ -157,29 +152,11 @@ public sealed abstract class GhostsController permits BlinkyController, ClydeCon
      * @return direction the ghost should go
      */
     public Direction waiting(Critter critter) {
-        switch (Constants.MAP_INDEX) {
-            case 1:
-                startPosition1 = new int[]{10, 9};
-                startPosition2 = new int[]{9, 9};
-                startPosition3 = new int[]{11, 9};
-                break;
-            case 2:
-                startPosition1 = new int[]{17, 19};
-                startPosition2 = new int[]{16, 19};
-                startPosition3 = new int[]{18, 19};
-                break;
-            case 3:
-                startPosition1 = new int[]{10, 1};
-                startPosition2 = new int[]{9, 1};
-                startPosition3 = new int[]{11, 1};
-                break;
-
-        }
         IntCoordinates pos = critter.getPos().round();
-        if (conditionOut() && pos.x() == Constants.INKY.x() && pos.y() == Constants.INKY.y()) {
+        if (conditionOut() && pos.x() == 10 && pos.y() == 9) {
             started = true;return Direction.NORTH;
-        } else if (pos.x() == Constants.BLINKY.x() && pos.y() == Constants.BLINKY.y()) return Direction.EAST;
-        else if (pos.x() == Constants.PINKY.x() && pos.y() == Constants.PINKY.y()) return Direction.WEST;
+        } else if (pos.x() == 9 && pos.y() == 9) return Direction.EAST;
+        else if (pos.x() == 11 && pos.y() == 9) return Direction.WEST;
         return critter.getDirection();
     }
 
--- src/main/java/model/PacMan.java
+++ src/main/java/model/PacMan.java
@@ -53,6 +53,8 @@ public final class PacMan implements Critter {
         // return INSTANCE;
     }
 
+    public static PacMan getPacMan(){return INSTANCE;}
+
     // Currently not used, so I commented this line
     // public String getName(){return PacMan.name;}
 
@@ -86,7 +88,7 @@ public final class PacMan implements Critter {
     }
 
     @Override
-    public int getSpeed() {
+    public double getSpeed() {
         // Changed so that when pacman is dying, it doesn't move anymore.
         return getIsDying()? 0:(isEnergized() ? 6 : 4);
     }
@@ -149,10 +151,6 @@ public final class PacMan implements Critter {
             }
 
             MazeState.getGridState()[pacPos.y()][pacPos.x()] = true;
-        }
-        if(MazeState.getFruitsGridState()[pacPos.y()][pacPos.x()] && !MazeState.allPointsCollected()){
-            MazeState.addScore(MazeState.getFruit(MazeState.id).getPoints());
-            MazeState.getFruitsGridState()[pacPos.y()][pacPos.x()] = false;
         }
         if (this.isDying) {
             this.deathTimerAni += (float) ((float) deltaT * 1E-9);
